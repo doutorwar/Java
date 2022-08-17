af3795ee83cced63202b958758c7914bf030e75c
Felipe P <felipepinheirocarneiro29@gmail.com>
2022-08-16 21:26:28 -0300

    Add files via upload

---
+++ Aula 2/Aluno.java
@@ -0,0 +1,7 @@
+package com.mycompany.aula2;
+
+public class Aluno extends Pessoa{
+    
+    String matricula;
+    
+}
---
+++ Aula 2/Principal.java
@@ -0,0 +1,15 @@
+package com.mycompany.aula2;
+
+public class Principal {
+    
+    public static void main(String[] args) {
+        System.out.println("TESTE");
+        
+        Pessoa p1 = new Pessoa();
+        p1.nome= "Felipe";
+        p1.sobrenome= "Pinheiro";
+        
+                System.out.println(p1.nome + " " + p1.sobrenome);
+    }
+    
+}
