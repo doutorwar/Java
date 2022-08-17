31e39007e94b1f474b19e97598a6a137a0496c65
Felipe P <felipepinheirocarneiro29@gmail.com>
2022-08-16 21:18:37 -0300

    Add files via upload

---
+++ Aula1/Carro.java
@@ -0,0 +1,15 @@
+package com.mycompany.aula1;
+
+class Carro {
+    
+    String roda;
+    String motor;
+    String cor;
+    
+    void acelerar() {
+        
+    }
+    void diminuir() {
+        
+    }
+}
