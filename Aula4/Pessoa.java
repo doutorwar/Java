c7d05d033a19d00bbf0c4db2ae83aec9bc4bdc78
Felipe Pinheiro <felipepinheirocarneiro29@gmail.com>
2022-09-06 20:15:46 -0300

    Update Pessoa.java

--- Aula4/Pessoa.java
+++ Aula4/Pessoa.java
@@ -1,21 +1,15 @@
-
 package com.mycompany.colecoesaula3;
-
 
 public abstract class Pessoa extends Object{
     
     private String nome;
 
-  
     public String getNome() {
         return nome;
     }
 
-    
     public void setNome(String nome) {
         this.nome = nome;
     }
-
-    
     
 }
