4fa8329cd5fec408269290a8ddc4cbc3835186e8
doutorwar <felipepinheirocarneiro29@gmail.com>
2023-07-26 00:44:38 -0300

    adiciona projeto de 3 aulas finalizadas

---
+++ Cursos/MeuPrimeiroProjeto/app/src/main/java/MeuPrimeiroProjeto/Main.java
@@ -0,0 +1,22 @@
+package MeuPrimeiroProjeto;
+
+public class Main {
+
+    //comentario de linha
+    
+    /*
+        linha
+        oi
+        bom dia
+        (nao vai pro javadoc)
+    */
+    
+    /** 
+     comentario de documentacao (pode virar um documento pra pessoa consultar, quando for gerado uma documenta�ao para o algoritmo)
+     (vai pro javadoc)
+    */
+    
+    public static void main(String[] args) {
+        System.out.println("Ola mundo");
+    }
+}
---
+++ Cursos/VariaveisTiposOperadores/app/src/main/java/VariaveisTiposOperadores/Main.java
@@ -0,0 +1,36 @@
+package VariaveisTiposOperadores;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        //inteiro pra guardar idade
+        int idade = 0;
+        //armazenar numeros reais (com virgula) (o f no final serve pra indicar que e uma variavel do tipo float)
+        float cotacaoDolar = 5.0f;
+        //armazenar numeros com casas decimais (o D no final serve pra indicar que e uma variavel do tipo double)
+        double cotacaoEuro = 6.0d;
+        //armazena apenas uma letra
+        char genero = 'M';
+        //armazenamento numerico
+        byte ponto = 0;
+        //verdadeiro ou falso
+        boolean estaCadastrado = falso;
+        
+        String nome = "Esse e um texto de uma variavel String";
+
+        //operadores aritimeticos
+        //pode usar mais de um operador porem tem que ter cuidado na ordem dos operadores
+        
+        //soma +
+        
+        //subtracao -
+        
+        //divisao /
+        
+        //multiplicacao *       
+        
+        //resto da divisao %       
+                
+    }
+}
