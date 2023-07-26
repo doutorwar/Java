fc63233df28836e81bc448df9fb58b4ec168db45
doutorwar <felipepinheirocarneiro29@gmail.com>
2023-07-26 12:16:48 -0300

    Adiciona 2 pastas com arquivos das aulas finalizadas

---
+++ Cursos/EstruturaCondicioalSe/app/src/main/java/EstruturaCondicioalSe/Main.java
@@ -0,0 +1,32 @@
+package EstruturaCondicioalSe;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        //operadores relacionais
+            // > maior
+            // >= maior ou igual
+            // < menor
+            // <= menor ou igual
+            // ==  igual igual (comparacao)
+            // != diferente
+        
+        //operadores logicos
+            // && (E)
+            // || (Ou)
+            // ! (negacao)
+        
+        int media = 7;
+        
+        if(media >= 7){
+            if(media == 10){
+            //aprovado com nota maxima
+            } else {
+            //aprovado
+            }
+        } else {
+            //reprovado
+        }
+        
+    }
+}
